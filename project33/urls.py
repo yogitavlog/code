0fe89f9e06e2253552364be1e17534a1013c0f7d
yogitavlog <67826345+yogitavlog@users.noreply.github.com>
2020-08-22 21:32:06 +0530

    Update urls.py

--- project33/urls.py
+++ project33/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import patterns, url
+from django.conf.urls import path, url
 from . import views
 
 urlpatterns = [
