f0c6d845c14464c5721d6cd2236c8835b96ead09
yogitavlog <67826345+yogitavlog@users.noreply.github.com>
2020-08-22 16:22:57 +0530

    Update settings.py

--- project33/settings.py
+++ project33/settings.py
@@ -16,6 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'template')
 
+STATIC_URL = '/static/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
